e8a09a0ba2042e8b6ca76b15439fa253a1474af8
OverlordMIke <mrl20894@gmail.com>
2025-12-04 04:45:04 +0000

    Done for the day

--- main-me-v1.py
+++ main-me-v1.py
@@ -1,6 +1,5 @@
 import pygame
 import math, random
-from dataclasses import dataclass
 from rocket_presets import ROCKET_PRESETS
 
 
@@ -13,6 +12,7 @@ def activate_particle(x,y,vx,vy,colour,life,decay,gravity:float=0.07):
             particle_vy[i] = vy
             particle_colour[i] = colour
             particle_life[i] = life
+            particle_max_life[i] = life
             particle_decay[i] = decay
             particle_gravity[i] = gravity
             particle_active[i] = True
@@ -25,14 +25,6 @@ def launch_rocket(rocket_name: str, pos_x: int):
     
     preset = ROCKET_PRESETS[rocket_name]
 
-    pattern_red = preset["theme"]["pattern"][0]
-    pattern_green = preset["theme"]["pattern"][1]
-    pattern_blue = preset["theme"]["pattern"][2]
-
-    if pattern_red == "random": pattern_red = random.randrange(255)
-    if pattern_green == "random": pattern_green = random.randrange(255)
-    if pattern_blue == "random": pattern_blue = random.randrange(255)
-
     rockets.append({
         "x": pos_x,
         "y": canvas_height,
@@ -41,11 +33,7 @@ def launch_rocket(rocket_name: str, pos_x: int):
         "theme": {
             "body": preset["theme"]["body"],
             "exhaust": preset["theme"]["exhaust"],
-            "pattern": (
-                pattern_red,
-                pattern_green,
-                pattern_blue
-            )
+            "pattern": preset["theme"]["pattern"]
         },
         "fuel": preset["fuel"],
         "thrust_strength": preset["thrust_strength"],
@@ -56,28 +44,37 @@ def launch_rocket(rocket_name: str, pos_x: int):
 
 def explode(rocket):
     cx, cy = rocket["x"], rocket["y"]
-    colour = rocket["theme"]["pattern"]
-    num_particles = random.randint(80, 140)
+    pattern = rocket["theme"]["pattern"]
+    for layer in pattern:
+        num_particles = layer["particle_count"] + random.randint(-layer["count_randomness"], layer["count_randomness"])
 
-    for _ in range(num_particles):
-        angle = random.uniform(0, math.tau)
-        speed = random.uniform(0.5, 30.0)
-        activate_particle(
-            cx,
-            cy,
-            math.cos(angle) * speed,
-            math.sin(angle) * speed,
-            colour,
-            1.0,
-            random.uniform(0.5, 1.0),
-            7
-        )
+        pattern_red = layer["colour"][0]
+        pattern_green = layer["colour"][1]
+        pattern_blue = layer["colour"][2]
+
+        if pattern_red == "random": pattern_red = random.randrange(255)
+        if pattern_green == "random": pattern_green = random.randrange(255)
+        if pattern_blue == "random": pattern_blue = random.randrange(255)
+
+        for _ in range(num_particles):
+            angle = random.uniform(0, math.tau)
+            speed = random.uniform(layer["explosive_force_min"], layer["explosive_force_max"])
+            activate_particle(
+                cx,
+                cy,
+                math.cos(angle) * speed,
+                math.sin(angle) * speed,
+                (pattern_red, pattern_green, pattern_blue),
+                layer["life"],
+                layer["decay"] + random.uniform(-layer["decay_randomness"], layer["decay_randomness"]),
+                7
+            )
 
 def physics_update(delta):
     for r in rockets[:]:
         r["y"] += r["vy"] * delta
         r["x"] += r["vx"] * delta
-        r["vy"] += 30 * delta
+        r["vy"] += 60 * delta
         if r["fuel"] > 0: r["burning"] = True
         else: r["burning"] = False
         if r["burning"]:
@@ -87,7 +84,6 @@ def physics_update(delta):
                 r["x"],
                 r["y"],
                 r["vx"] + random.uniform(-6, 6),
-                #(r["vy"] * 0.9) + 
                 random.uniform(-6, 6),
                 r["theme"]["exhaust"],
                 1.0,
@@ -106,8 +102,8 @@ def physics_update(delta):
     for i in range(len(particle_active)):
         if particle_active[i]:
             particle_vy[i] += particle_gravity[i] * delta
-            if abs(particle_vx[i]) < abs(global_wind):
-                particle_vx[i] += global_wind * 0.06 * delta
+            particle_vx[i] *= 1 - 0.75 * delta
+            particle_vy[i] *= 1 - 0.75 * delta
             particle_x[i] += particle_vx[i] * delta
             particle_y[i] += particle_vy[i] * delta
             particle_life[i] += -particle_decay[i] * delta
@@ -117,7 +113,6 @@ def physics_update(delta):
 
 
 def draw_update(canvas_screen, screen, current_fps, delta):
-    global particle_peak
     canvas_screen.fill((5,5,20))
 
     for r in rockets:
@@ -125,7 +120,9 @@ def draw_update(canvas_screen, screen, current_fps, delta):
     
     for i in range(len(particle_active)):
         if particle_active[i]:
-            brightness = particle_life[i]
+            #fade_factor = particle_life[i] / particle_max_life[i]
+            #colour = tuple(int(c * fade_factor) for c in particle_colour[i])
+            brightness = particle_life[i] / particle_max_life[i]
             colour = tuple(min(255, int(c * brightness * 2.2)) for c in particle_colour[i])
             canvas_screen.set_at((round(particle_x[i]), round(particle_y[i])),colour)
     
@@ -136,27 +133,38 @@ def draw_update(canvas_screen, screen, current_fps, delta):
     screen.blit(screen_text, (10 * window_scale, 10 * window_scale))
     screen_text = pygame.font.Font(None, 24 * window_scale).render(f"Delta: {delta}", True, (255,255,255))
     screen.blit(screen_text, (10 * window_scale, 30 * window_scale))
+    screen_text = pygame.font.Font(None, 24 * window_scale).render(f"Current Rocket: {active_rocket}", True, (255,255,255))
+    screen.blit(screen_text, (10 * window_scale, 50 * window_scale))
 
     pygame.display.flip()
 
+def cycle_available_rockets(dir:int = 1):
+    global active_rocket_index, active_rocket
+    new_index = (active_rocket_index + dir) % len(available_rockets)
+    active_rocket = available_rockets[new_index]
+    active_rocket_index = new_index
 
-particle_max = 2000
+particle_max = 10_000
 particle_x = [0 for _ in range(particle_max)]
 particle_y = [0 for _ in range(particle_max)]
 particle_vx = [0 for _ in range(particle_max)]
 particle_vy = [0 for _ in range(particle_max)]
 particle_colour = [(0,0,0) for _ in range(particle_max)]
 particle_life = [0 for _ in range(particle_max)]
+particle_max_life = [0 for _ in range(particle_max)]
 particle_decay = [0 for _ in range(particle_max)]
 particle_gravity = [0.07 for _ in range(particle_max)]
 particle_active = [False for _ in range(particle_max)]
 
-canvas_width, canvas_height = 800, 600
-window_scale = 1
+canvas_width, canvas_height = 854, 480
+window_scale = 2
 rockets = []
 last_launch_time = 0
-particle_peak = 0
 global_wind = 10
+
+available_rockets = list(ROCKET_PRESETS.keys())
+active_rocket = available_rockets[0]
+active_rocket_index = 0
 
 def main():
     global last_launch_time
@@ -176,19 +184,19 @@ def main():
     while running:
         fps = clock.get_fps()
         current_time = pygame.time.get_ticks()
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                launch_pos = random.uniform(40, canvas_width-40)
-                launch_rocket("ClassicRandom", launch_pos)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    launch_rocket("ClassicRed", canvas_width/4)
-                if event.key == pygame.K_w:
-                    launch_rocket("ClassicWhite", canvas_width/4*2)
-                if event.key == pygame.K_b:
-                    launch_rocket("ClassicBlue", canvas_width/4*3)
+                if event.button == 1:
+                    mouse_x, mouse_y = event.pos
+                    launch_pos = mouse_x / window_scale
+                    launch_rocket(active_rocket, launch_pos)
+                if event.button == 4:
+                    cycle_available_rockets(1)
+                if event.button == 5:
+                    cycle_available_rockets(-1)
         
         #if (current_time - last_launch_time) >= 500:
         #    last_launch_time = current_time
@@ -197,6 +205,7 @@ def main():
 
         physics_update(delta)
         draw_update(canvas_screen, screen, fps, delta)
+
         delta = clock.tick(60) / 1000
 
 if __name__ == "__main__":
--- rocket_presets.py
+++ rocket_presets.py
@@ -5,70 +5,124 @@ ROCKET_PRESETS = {
         "theme": {
             "body": (255,255,255),
             "exhaust": (240, 201, 0),
-            "pattern": (
-                "random",
-                "random",
-                "random"
-            )
+            "pattern": [
+                {
+                    "particle_count": 110,
+                    "count_randomness": 30,
+                    "colour": ("random","random","random"),
+                    "explosive_force_min": 0.5,
+                    "explosive_force_max": 40,
+                    "life": 1.0,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                }
+            ]
         },
         "fuel": 1,
         "thrust_strength": 180,
-        "fuse": 2,
+        "fuse": 3,
     },
-    "ClassicRed": {
+    "Flower": {
         "theme": {
             "body": (255,255,255),
             "exhaust": (240, 201, 0),
-            "pattern": (
-                255,
-                0,
-                0
-            )
+            "pattern": [
+                {
+                    "particle_count": 110,
+                    "count_randomness": 30,
+                    "colour": ("random","random","random"),
+                    "explosive_force_min": 0.5,
+                    "explosive_force_max": 40,
+                    "life": 4,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                },
+                {
+                    "particle_count": 60,
+                    "count_randomness": 30,
+                    "colour": (255, 178, 0),
+                    "explosive_force_min": 0.5,
+                    "explosive_force_max": 10,
+                    "life": 4,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                }
+            ]
         },
-        "fuel": 7,
-        "thrust_strength": 9,
-        "fuse": 15,
+        "fuel": 1,
+        "thrust_strength": 180,
+        "fuse": 3,
     },
-    "ClassicGreen": {
+    "TwoTime": {
         "theme": {
             "body": (255,255,255),
             "exhaust": (240, 201, 0),
-            "pattern": (
-                0,
-                255,
-                0
-            )
+            "pattern": [
+                {
+                    "particle_count": 110,
+                    "count_randomness": 30,
+                    "colour": ("random","random","random"),
+                    "explosive_force_min": 0.5,
+                    "explosive_force_max": 40,
+                    "life": 4,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                },
+                {
+                    "particle_count": 110,
+                    "count_randomness": 30,
+                    "colour": ("random","random","random"),
+                    "explosive_force_min": 35,
+                    "explosive_force_max": 80,
+                    "life": 4,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                }
+            ]
         },
-        "fuel": 7,
-        "thrust_strength": 9,
-        "fuse": 15,
+        "fuel": 1,
+        "thrust_strength": 180,
+        "fuse": 3,
     },
-    "ClassicBlue": {
+    "Patriot": {
         "theme": {
             "body": (255,255,255),
             "exhaust": (240, 201, 0),
-            "pattern": (
-                0,
-                0,
-                255
-            )
+            "pattern": [
+                {
+                    "particle_count": 200,
+                    "count_randomness": 30,
+                    "colour": (255,255,255),
+                    "explosive_force_min": 0.5,
+                    "explosive_force_max": 60,
+                    "life": 3.5,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                },
+                {
+                    "particle_count": 90,
+                    "count_randomness": 30,
+                    "colour": (10,10,255),
+                    "explosive_force_min": 5,
+                    "explosive_force_max": 40,
+                    "life": 3,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                },
+                {
+                    "particle_count": 300,
+                    "count_randomness": 30,
+                    "colour": (255,10,10),
+                    "explosive_force_min": 35,
+                    "explosive_force_max": 60,
+                    "life": 3,
+                    "decay": 0.75,
+                    "decay_randomness": 0.25
+                }
+            ]
         },
-        "fuel": 7,
-        "thrust_strength": 9,
-        "fuse": 15,
+        "fuel": 3,
+        "thrust_strength": 90,
+        "fuse": 4.5,
     },
-    "ClassicWhite": {
-        "theme": {
-            "body": (255,255,255),
-            "exhaust": (240, 201, 0),
-            "pattern": (
-                255,
-                255,
-                255
-            )
-        },
-        "fuel": 7,
-        "thrust_strength": 9,
-        "fuse": 15,
-    }
 }
